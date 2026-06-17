a59ac9f3422ac097a6d42ef0a10236ae31306a2b
kvbqq <kvbqqq@gmail.com>
2026-06-17 22:19:43 +0200

    chore: inital setup

---
+++ backend/src/test/java/com/kvbqq/quizarena/QuizarenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvbqq.quizarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizarenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
